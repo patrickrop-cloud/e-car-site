510dedd47822258a660f66e00d48a1e9b72d4fc6
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-04-28 13:12:21 +0300

    Add functionality to car images and detais in js

--- js/content.js
+++ js/content.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-    $(".btn-primary").click(function(){
-        $(".model").toggle();
+    $("#dropdown1").click(function(){
+        $("#model1").toggle();
     });
+    $("#dropdown2").click(function(){
+        $("#model2").toggle();
+    });
+
+    $("#car1").hover(function(){
+        $("#car1>.details").toggle();
+    })
 })
